0393826b0c39e258a0e84c369750734d16726da8
SMNahuel <nahuelsan96@gmail.com>
2021-12-06 19:18:00 -0300

    feat : responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './components/Home/Home';
+import "./sass/style.scss";
 
 function App() {
   return (
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import style from "./Home.module.css";
 import Egg from "../../assets/desktop/image-transform.jpg"
 import Cup from "../../assets/desktop/image-stand-out.jpg"
 import Blueberry from "../../assets/desktop/image-graphic-design.jpg"
@@ -12,14 +11,19 @@ import Arrow from "../../assets/design/icon-arrow-down.svg"
 import Emily from "../../assets/design/image-emily.jpg"
 import Jennie from "../../assets/design/image-jennie.jpg"
 import Thomas from "../../assets/design/image-thomas.jpg"
+import Facebook from "../../assets/icons/icon-facebook.svg"
+import Instagram from "../../assets/icons/icon-instagram.svg"
+import Twitter from "../../assets/icons/icon-twitter.svg"
+import Pinterest from "../../assets/icons/icon-pinterest.svg"
 
 const Home = () => {
     return (
-        <div>
-            <div className={style.ContainerHeader}>
-                <div className={style.Navbar}>
-                    <p className={style.Sunny} href="#">sunnyside</p>
-                    <div className={style.ButonNav}>
+        <>
+        {/* -----------------------------Header ----------------------------- */}
+            <div className="ContainerHeader">
+                <div className="d-none d-lg-flex Navbar">
+                    <p className="Sunny" href="#">sunnyside</p>
+                    <div className="ButonNav">
                         <p>About    </p>
                         <p>Services </p>
                         <p>Proyects </p>
@@ -27,53 +31,59 @@ const Home = () => {
                     </div>
                 </div>
                 <h2>WE ARE CREATIVES</h2>
-                <div className={style.Arrow}>
+                <div className="Arrow">
                     <img src={Arrow} alt="arrow-down" />
                 </div>
             </div>
 
-            <div class="d-flex flex-direction-row">
-                <div class="w-50 d-flex flex-column justify-content-center align-items-left p-4 m-4">
-                    <h2 className={style.Title}>Transform your brand</h2>
+            {/* ----------------------------- Egg  -----------------------------*/}
+            <div className="d-flex flex-column-reverse flex-sm-column-reverse flex-md-column-reverse flex-lg-row">
+                <div className="d-flex flex-column justify-content-center textContainer">
+                    <h2 className="Title">Transform your brand</h2>
                     <p>We are a full-service creative agency specializing in helping brands grow fast.
                         Engage your clients through compelling visuals that do most of the marketing for you.
                     </p>
-                    <p className={style.Learn}>Learn more <hr className={style.BorderYellow} /></p>
+                    <p className="Learn">Learn more</p>
+                    <hr className="BorderYellow" />
                 </div>
-                <img src={Egg} className={style.Header} class="w-50" alt="Header" />
+                <img src={Egg} alt="egg" className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6" />
             </div>
 
-            <div class="d-flex flex-direction-row">
-                <img src={Cup} className={style.Header} class="w-50" alt="Ilustrator" />
-                <div class="w-50 d-flex flex-column justify-content-center align-items-left p-4 m-4">
-                    <h2 className={style.Title}>Stand out to the right audience</h2>
+            {/* ----------------------------- Cup ----------------------------- */}
+            <div className="d-flex flex-column flex-sm-column flex-md-column flex-lg-row">
+                <img src={Cup} className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6" alt="Ilustrator" />
+                <div className="d-flex flex-column justify-content-center textContainer">
+                    <h2 className="Title">Stand out to the right audience</h2>
                     <p>Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. </p>
-                    <p className={style.Learn}>Learn more <hr className={style.BorderRed} /></p>
-
+                    <p className="Learn">Learn more </p>
+                    <hr className="BorderRed" />
                 </div>
             </div>
 
-            <div class="d-flex flex-direction-row">
-                <img src={Blueberry} className={style.Header} alt="Ilustrator" class="w-50" />
-                <img src={Orage} className={style.Header} alt="Ilustrator" class="w-50" />
+            {/* ----------------------------- Orange Blueberry -----------------------------*/}
+            <div className="d-flex flex-column flex-sm-column flex-md-column flex-lg-row">
+                <img src={Blueberry} className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6 Header" alt="Ilustrator"  />
+                <img src={Orage}  className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 col-xxl-6 Header" alt="Ilustrator" />
             </div>
 
-            <div class="container d-flex flex-column justify-content-center align-items-center">
-                <h2 className={style.Client}>Client testimonials</h2>
-                <div class="d-flex flex-direction-row p-4">
-                    <div className={style.Testimonial}>
+
+            {/* ----------------------------- Testimonial -----------------------------*/}
+            <div className="container d-flex flex-column flex-sm-column flex-md-column flex-lg-column justify-content-center align-items-center">
+                <h2 className="Client">Client testimonials</h2>
+                <div className="d-flex flex-column flex-sm-row flex-md-row flex-lg-row">
+                    <div className="Testimonial row">
                         <img src={Emily} alt="Client" />
                         <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
                         <h3>Emily R.</h3>
                         <p>Marketing Director</p>
                     </div>
-                    <div className={style.Testimonial}>
+                    <div className="Testimonial row">
                         <img src={Thomas} alt="Client" />
                         <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
                         <h3>Thomas S.</h3>
                         <p>Chief Operating Officer</p>
                     </div>
-                    <div className={style.Testimonial}>
+                    <div className="Testimonial row ">
                         <img src={Jennie} alt="Client" />
                         <p>We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.</p>
                         <h3>Jennie F.</h3>
@@ -82,17 +92,32 @@ const Home = () => {
                 </div>
             </div>
 
-            <div class="d-flex flex-direction-row">
-                <img src={Milks} class="col-3" alt="Client" />
-                <img src={OrangePlate} class="col-3" alt="Client" />
-                <img src={Cone} class="col-3" alt="Client" />
-                <img src={Sugar} class="col-3" alt="Client" />
+            <div className="d-flex">
+                <img src={Milks} className="col-3" alt="Client" />
+                <img src={OrangePlate} className="col-3" alt="Client" />
+                <img src={Cone} className="col-3" alt="Client" />
+                <img src={Sugar} className="col-3" alt="Client" />
             </div>
+            
+            {/* ----------------------------- Footer -----------------------------*/}
+            <div className="Footer">
+                <div className="d-flex aling-items-center justify-content-center">
+                    <p className="SunnyFooter" href="#">sunnyside</p>
 
-            <div>
-
+                </div>
+                <div className="d-flex aling-items-center justify-content-center">
+                    <p className="footerP">About    </p>
+                    <p className="footerP">Services </p>
+                    <p className="footerP">Proyects </p>
+                </div>
+                <div className="d-flex aling-items-center justify-content-center">
+                    <img src={Facebook} className="ms-4" alt="icon-social-facebook" />
+                    <img src={Instagram} className="ms-4" alt="icon-social-instagram" />
+                    <img src={Twitter} className="ms-4" alt="icon-social-twitter" />
+                    <img src={Pinterest} className="ms-4" alt="icon-social-pinterest" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
